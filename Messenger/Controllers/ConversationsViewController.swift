2ff5e40c565101e46542f647a1e66c8649738965
OybekNarzikulov <oybeknarzikulov1999@gmail.com>
2022-11-27 14:40:02 +0500

    initial commit

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Oybek Narzikulov on 27/11/22.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
